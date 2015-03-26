f4a7d95044929b92ea8b871417f6ec75db3c636f
Fernando Rodriguez <frr@agbo.biz>
2015-03-26 11:27:34 +0100

    No hacía las llamadas a super correctamente (o no lo hacias). Eso es importante.

--- IAALibraryTableViewController.m
+++ IAALibraryTableViewController.m
@@ -38,7 +38,7 @@
 
 -(void) viewDidAppear:(BOOL)animated
 {
-    [super viewWillAppear:TRUE];
+    [super viewDidAppear:animated];
     // Alta en notificaciones
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(favoritesDidChange:)
@@ -50,6 +50,8 @@
 
 -(void) viewWillDisappear:(BOOL)animated
 {
+    [super viewWillDisappear:animated];
+    
     // Baja en notificaciones
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
