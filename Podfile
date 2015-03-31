5adad5b0dc9f604a3ea7f554993c352c7c1358fa
Iván Alexis Abad <treki23@gmail.com>
2015-03-31 17:54:32 +0200

    añadido todo lo de cocoa pods

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'HackerBooks' do
+  pod 'Reader', :git => 'https://github.com/vfr/Reader.git'
+end
---
+++ Pods/Target Support Files/Pods-HackerBooks-Reader/Pods-HackerBooks-Reader-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HackerBooks_Reader : NSObject
+@end
+@implementation PodsDummy_Pods_HackerBooks_Reader
+@end
